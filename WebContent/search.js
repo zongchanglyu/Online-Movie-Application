766de6a6718f1c821d906274620945d1d448ce85
ZONGCHANG LYU <zongchanglyu@ZONGCHANGdeMacBook-Pro.local>
2020-04-20 01:04:17 -0700

    update search.js and movie-list.html files, but still have some errors in js file

--- WebContent/search.js
+++ WebContent/search.js
@@ -2,51 +2,41 @@ let advanceSearch = $("#advance_search_form");
 
 
 
-function handleMoviesResult(resultData) {
-    console.log("handleMoviesResult: populating movie table from resultData");
+function handleSearchResult(resultData) {
+    console.log("handleResult: populating star info from resultData");
 
-    // Populate the movie table
+    // populate the star info h3
+    // find the empty h3 body by id "star_info"
+    // let starInfoElement = jQuery("#star_info");
+    //
+    // // append two html <p> created to the h3 body, which will refresh the page
+    // starInfoElement.append("<p>Star Name: " + resultData[0]["star_name"] + "</p>" +
+    //     "<p>Date Of Birth: " + resultData[0]["star_dob"] + "</p>");
+
+    console.log("handleResult: populating movie table from resultData");
+
+    // Populate the star table
     // Find the empty table body by id "movie_table_body"
-    let starTableBodyElement = jQuery("#movie_table_body");
+    let movieTableBodyElement = jQuery("#movie_table_body");
 
-    // Iterate through resultData, no more than 20 entries
-    for (let i = 0; i < Math.min(20, resultData.length); i++) {
-
-        // Concatenate the html tags with resultData jsonObject
+    // Concatenate the html tags with resultData jsonObject to create table rows
+    for (let i = 0; i < Math.min(10, resultData.length); i++) {
         let rowHTML = "";
         rowHTML += "<tr>";
-        rowHTML +=
-            "<th>" +
-            // Add a link to single-movie.html with id passed with GET url parameter
-            '<a href="single-movie.html?id=' + resultData[i]['movie_id'] + '">'
-            + resultData[i]["movie_title"] +     // display movie_title for the link text
-            '</a>' +
-            "</th>";
+        // rowHTML +=
+        //     "<th>" +
+        //     '<a href="single-movie.html?id=' + resultData[i]['movie_id'] + '">'
+        //     + resultData[i]["movie_title"] +
+        //     '</a>' +
+        //     "</th>";
+
+        rowHTML += "<th>" + resultData[i]["movie_title"] + "</th>";
         rowHTML += "<th>" + resultData[i]["movie_year"] + "</th>";
-        rowHTML += "<th>" + resultData[i]["director"] + "</th>";
-
-        rowHTML += "<th>";
-        for(let g = 0; g < Math.min(3, resultData[i]["genres_name"].length); g++){
-            rowHTML += resultData[i]["genres_name"][g] + "<br>";
-        }
-        rowHTML = rowHTML.substring(0, rowHTML.length - 4);
-        rowHTML += "</th>";
-
-        rowHTML += "<th>";
-        for(let s = 0; s < Math.min(3, resultData[i]["stars_name"].length); s++){
-            // Add a link to single-movie.html with id passed with GET url parameter
-            rowHTML += '<a href="single-star.html?id=' + resultData[i]["stars_name"][s]["star_id"] + '">'
-            rowHTML += resultData[i]["stars_name"][s]["star_name"] + "<br>";
-        }
-        rowHTML = rowHTML.substring(0, rowHTML.length - 4);
-        rowHTML += "</th>";
-
-        rowHTML += "<th>" + resultData[i]["rating"] + "</th>";
+        rowHTML += "<th>" + resultData[i]["movie_director"] + "</th>";
         rowHTML += "</tr>";
 
         // Append the row created to the table body, which will refresh the page
-        starTableBodyElement.append(rowHTML);
-    }
+        movieTableBodyElement.append(rowHTML);
 }
 
 function handleSearchInfo(searchEvent) {
@@ -61,7 +51,7 @@ function handleSearchInfo(searchEvent) {
     $.ajax("api/search", {
         method: "GET",
         data: advanceSearch.serialize(),
-        success: handleMoviesResult
+        success: handleSearchResult
     });
 }
 
