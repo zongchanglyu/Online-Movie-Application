2ca8650878dd6315a17d8876cf95c8a58f9d3505
mingwandowski <haoming2019@gmail.com>
2020-04-20 20:28:36 -0400

    session version worked, adv search result will be set into the session and redirect to movie-list page, and the movie-list page will always look for result from the session

---
+++ src/MovieListServlet.java
@@ -0,0 +1,60 @@
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+import javax.annotation.Resource;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+// Declaring a WebServlet called MovieListServlet, which maps to url "/api/movie-list"
+@WebServlet(name = "MovieListServlet", urlPatterns = "/api/movie-list")
+public class MovieListServlet extends HttpServlet {
+    private static final long serialVersionUID = 2L;
+
+    // Create a dataSource which registered in web.xml
+    @Resource(name = "jdbc/moviedb")
+    private DataSource dataSource;
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+     * response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        // Output stream to STDOUT
+        PrintWriter out = response.getWriter();
+
+        // Get a instance of current session on the request
+        HttpSession session = request.getSession();
+        JsonArray previousList = (JsonArray)session.getAttribute("previousList");
+
+        try{
+            // write JSON string to output
+            out.write(previousList.toString());
+            System.out.println("write previousList to out");
+            // set response status to 200 (OK)
+            response.setStatus(200);
+        }catch (Exception e) {
+            // write error message JSON object to output
+            JsonObject jsonObject = new JsonObject();
+            jsonObject.addProperty("errorMessage", e.getMessage());
+            out.write(jsonObject.toString());
+
+            // set reponse status to 500 (Internal Server Error)
+            response.setStatus(500);
+        }
+        out.close();
+        //close it;
+    }
+
+}
