0d2653a75c39115d54f1a0f32b32d0fef4780fa2
mingwandowski <haoming2019@gmail.com>
2020-05-11 18:56:31 -0400

    duplicate data bug and some display bug fixed

--- src/SingleStarServlet.java
+++ src/SingleStarServlet.java
@@ -45,7 +45,7 @@ public class SingleStarServlet extends HttpServlet {
 			Connection dbcon = dataSource.getConnection();
 
 			// Construct a query with parameter represented by "?"
-			String query = "SELECT * from stars as s, stars_in_movies as sim, movies as m " +
+			String query = "SELECT DISTINCT * from stars as s, stars_in_movies as sim, movies as m " +
 					"where m.id = sim.movieId and sim.starId = s.id and s.id = ? " +
 					"order by year desc, title asc ;";
 
