c595812b0bda85d3c992546d3ead3eaedf4fac9a
mingwandowski <haoming2019@gmail.com>
2020-05-11 15:35:00 -0400

    fixed xml bugs

---
+++ src/main/java/ParseStarsInMovies.java
@@ -0,0 +1,139 @@
+package main.java;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class ParseStarsInMovies {
+    // field
+    // map<starName, movieName>
+    static Map<String, String> myStarsInMovies;
+    Document dom;
+    static Set<String[]> myNewStarsInMovies;
+
+    // constructor
+    public ParseStarsInMovies() {
+        myStarsInMovies = new HashMap<>();
+        myNewStarsInMovies = new HashSet<>();
+    }
+
+    // getter and setter
+    public Map<String, String> getMyStarsInMovies(){
+        return myStarsInMovies;
+    }
+
+    public Set<String[]> getMyNewStarsInMovies(){
+        return myNewStarsInMovies;
+    }
+
+    // methods
+    public void run() throws SQLException, IllegalAccessException, ClassNotFoundException, InstantiationException {
+        //parse the xml file and get the dom object
+        parseXmlFile();
+
+        //get each employee element and create a Employee object
+        parseDocument();
+
+        //Iterate through the list and print the data
+        printData();
+
+        System.out.println("begin to insertData.....");
+//        insertData();
+    }
+
+    private void parseXmlFile() {
+        //get the factory
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+
+        try {
+
+            //Using factory get an instance of document builder
+            DocumentBuilder db = dbf.newDocumentBuilder();
+
+            //parse using builder to get DOM representation of the XML file
+            dom = db.parse("casts124.xml");
+
+        } catch (ParserConfigurationException pce) {
+            pce.printStackTrace();
+        } catch (SAXException se) {
+            se.printStackTrace();
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+    }
+
+    private void parseDocument(){
+        //get the root elememt
+        Element docEle = dom.getDocumentElement();
+
+        //get a nodelist of <employee> elements
+        NodeList nl = docEle.getElementsByTagName("dirfilms");
+        if (nl != null && nl.getLength() > 0){
+            for (int i = 0; i < nl.getLength(); i++){
+                Element el = (Element) nl.item(i);
+                NodeList filmc = el.getElementsByTagName("filmc");
+                if (filmc != null && filmc.getLength() > 0){
+                    for (int j = 0; j < filmc.getLength(); j++){
+                        Element f = (Element) filmc.item(j);
+
+                        NodeList m = f.getElementsByTagName("m");
+
+                        if (m != null && m.getLength() > 0){
+                            for (int k = 0; k < m.getLength(); k++){
+                                Element M = (Element) m.item(k);
+
+                                String movieName = getTextValue(M, "t");
+                                String stageName = getTextValue(M, "a");
+
+                                if("Forrest Gump".equals(movieName))
+                                    System.out.println("Forrest Gump in xml, actor is: " + stageName);
+
+                                if(stageName==null || movieName==null) continue;
+
+                                myStarsInMovies.put(stageName, movieName);
+                                myNewStarsInMovies.add(new String[]{stageName, movieName});
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private String getTextValue(Element ele, String tagName) {
+        String textVal = null;
+        NodeList nl = ele.getElementsByTagName(tagName);
+        if (nl != null && nl.getLength() > 0) {
+            Element el = (Element) nl.item(0);
+            if(el.getFirstChild()!=null){
+                textVal = el.getFirstChild().getNodeValue();
+            }
+        }
+        return textVal;
+    }
+
+    private void printData() {
+
+        System.out.println("numbers of data '" + myStarsInMovies.size() + "'.");
+        System.out.println("numbers of new data '" + myNewStarsInMovies.size() + "'.");
+
+//       for(String s: myStarsInMovies.keySet()){
+//           System.out.println("star is: "+s);
+////           for(String id: myStarsInMovies.get(s)){
+////               System.out.println("Star is: "+s+", movieId is: "+id);
+////
+////           }
+//       }
+    }
+}
--- src/main/java/XmlParse.java
+++ src/main/java/XmlParse.java
@@ -8,15 +8,8 @@ public class XmlParse {
     public static void main(String[] args) throws ClassNotFoundException, SQLException, InstantiationException, IllegalAccessException {
 
         /**
-         * parse movies into database
+         * connect to database
          * */
-
-        ParseMovie pm = new ParseMovie();
-
-        pm.run();
-
-        List<Movies> myMovies = pm.getMyMovies();
-        Set<String> allKindsOfGenres = pm.getAllKindsOfGenres();
 
         System.out.println("connect to database....");
 
@@ -31,16 +24,28 @@ public class XmlParse {
             e.printStackTrace();
         }
 
+        /**
+         * parse movies into database
+         * */
+        System.out.println("parse movies into database");
+
+        ParseMovie pm = new ParseMovie();
+        pm.run();
+        List<Movies> myMovies = pm.getMyMovies();
+        Set<String> allKindsOfGenres = pm.getAllKindsOfGenres();
+
+
         // oldMovies<director, tmpMovie<title, year>>
         Map<String, Map<String, Integer>> oldMovies = new HashMap<>();
-
         // avoid duplicate movieId
         Set<String> movieId = new HashSet<>();
+        // save all movieTitle
+        Map<String, String> movieTitleId = new HashMap<>();
+//        Set<String> movieTitle = new HashSet<>();
 
         String query = "select * from movies";
 
         PreparedStatement statement = conn.prepareStatement(query);
-
         ResultSet result = statement.executeQuery();
 
         while (result.next()) {
@@ -49,6 +54,8 @@ public class XmlParse {
             oldMovies.put(result.getString("director"), tempMovie);
 
             movieId.add(result.getString("id"));
+//            movieTitle.add(result.getString("title"));
+            movieTitleId.put(result.getString("title"), result.getString("id"));
         }
 
         conn.setAutoCommit(false);
@@ -83,12 +90,16 @@ public class XmlParse {
 
         for (Movies movie : myMovies) {
             if (oldMovies.containsKey(movie.getDirector()) && oldMovies.get(movie.getDirector()).containsKey(movie.getTitle()) || movie.getId() == null) {
+//                System.out.println("duplicate movie: " + movie.getTitle());
                 continue;
             }
 
             while (movieId.contains(movie.getId()))
                 movie.setId(movie.getId() + "1");
             movieId.add(movie.getId());
+
+//            movieTitle.add(movie.getTitle());
+            movieTitleId.put(movie.getTitle(), movie.getId());
 
             psInsertRecord.setString(1, movie.getId());
             psInsertRecord.setString(2, movie.getTitle());
@@ -127,13 +138,13 @@ public class XmlParse {
          * */
 
         ParseStar ps = new ParseStar();
-
         ps.run();
-
         Set<Stars> myStars = ps.getMyStars();
-
-
+        // map<starName, dob>
         Map<String, Integer> oldStars = new HashMap<>();
+        // map<starName, starId>
+        Map<String, String> starNameId = new HashMap<>();
+
 
         query = "select * from stars;";
 
@@ -142,20 +153,12 @@ public class XmlParse {
         result = statement.executeQuery();
 
         while(result.next()){
-            try{
-                String name = result.getString("name");
-                Integer birthYear = result.getInt("birthYear");
-                oldStars.put(name, birthYear);
-            }catch(Exception e){
-                System.out.println("get name or birthYear failed");
-            }
-//            try{
-////                oldStars.put(result.getString("name"), Integer.parseInt(result.getString("year")));
-////                successful++;
-////            }catch (Exception e){
-////                oldStars.put(result.getString("name"), 0);
-////                failed++;
-////            }
+            String name = result.getString("name");
+            Integer birthYear = result.getInt("birthYear");
+            oldStars.put(name, birthYear);
+
+            String id = result.getString("id");
+            starNameId.put(name, id);
         }
 
         System.out.println("old stars size is: " + oldStars.size());
@@ -177,7 +180,6 @@ public class XmlParse {
 
         for(Stars star : myStars){
             if(oldStars.containsKey(star.getName()) && oldStars.get(star.getName())==star.getBirthYear()){
-                System.out.println("duplicate star: " + star.getName());
                 continue;
             }
 
@@ -188,6 +190,7 @@ public class XmlParse {
             statement.setInt(3, star.getBirthYear());
 
             oldStars.put(star.getName(), star.getBirthYear());
+            starNameId.put(star.getName(), id);
 
             statement.addBatch();
         }
@@ -196,6 +199,130 @@ public class XmlParse {
         conn.commit();
 
         System.out.println("insert stars data into db finished!!!");
+
+        /**
+         * parse stars_in_movies into database
+         * */
+
+        ParseStarsInMovies psim = new ParseStarsInMovies();
+        psim.run();
+        Map<String, String> myStarsInMovies = psim.getMyStarsInMovies();
+        System.out.println("myStarsInMovies size is: "+ myStarsInMovies.size());
+        Set<String[]> myNewStarsInMovies = psim.getMyNewStarsInMovies();
+        System.out.println("myNewStarsInMovies size is: "+ myNewStarsInMovies.size());
+
+        query = "select max(id) as id from stars";
+        statement = conn.prepareStatement(query);
+        rs = statement.executeQuery();
+        rs.next();
+
+        starId = rs.getString("id");
+        prefix = starId.substring(0, 2);
+        number = Integer.parseInt(starId.substring(2));
+
+
+        conn.setAutoCommit(false);
+/*
+        //just in case of some extra new stars data in casts124.xml, so we want to insert stars again.
+        String starInsertQuery = "insert into stars (id, name, birthYear) values (?,?,?) ";
+        PreparedStatement starInsertStatement = conn.prepareStatement(starInsertQuery);
+
+        query = "insert into stars_in_movies (starId, movieId) " +
+                "values ((select id from stars where name = ? limit 1), (select id from movies where title = ? limit 1))";
+        statement = conn.prepareStatement(query);
+
+        String tmpQuery = "insert into stars_in_movies (starId, movieId) " +
+                          "values( ? , (select id from movies where title = ? limit 1));";
+        PreparedStatement tmpQueryStatement = conn.prepareStatement(tmpQuery);
+
+        int count = 0;
+
+        for(String starName: myStarsInMovies.keySet()){
+            if(movieTitle.contains(myStarsInMovies.get(starName))){
+                if("Forrest Gump".equals(myStarsInMovies.get(starName))) System.out.println("Forrest Gump found");
+                if("Tom Hanks".equals(starName)) System.out.println("Tom Hanks found");
+                if(!oldStars.containsKey(starName)){
+                    String tmpStarId = prefix + (++number);
+                    starInsertStatement.setString(1, tmpStarId);
+                    starInsertStatement.setString(2, starName);
+                    starInsertStatement.setInt(3,0);
+                    starInsertStatement.addBatch();
+
+                    oldStars.put(starName, 0);
+
+                    tmpQueryStatement.setString(1, tmpStarId);
+                    tmpQueryStatement.setString(2, myStarsInMovies.get(starName));
+                    tmpQueryStatement.addBatch();
+                }else{
+                    statement.setString(1, starName);
+                    statement.setString(2, myStarsInMovies.get(starName));
+                    statement.addBatch();
+                }
+            }
+            else{
+                // maybe a new movie, not sure add to db or not
+                count++;
+
+//                System.out.println("movie title not found: " + myStarsInMovies.get(starName));
+            }
+        }
+        System.out.println("sum of movie title not fount: " + count + "\nmaybe a new movie, not sure add to db or not");
+*/
+
+        String starInsertQuery = "insert into stars (id, name, birthYear) values (?,?,?) ";
+        PreparedStatement starInsertStatement = conn.prepareStatement(starInsertQuery);
+
+        query = "insert into stars_in_movies (starId, movieId) " +
+                "values (? , ? )";
+        statement = conn.prepareStatement(query);
+
+        String tmpQuery = "insert into stars_in_movies (starId, movieId) " +
+                "values( ? , ? );";
+        PreparedStatement tmpQueryStatement = conn.prepareStatement(tmpQuery);
+
+        int count = 0;
+
+        for(String[] StarName_MovieName : myNewStarsInMovies){
+            if(movieTitleId.containsKey(StarName_MovieName[1])){
+//                if("Forrest Gump".equals(StarName_MovieName[1])) System.out.println("Forrest Gump found");
+//                if("Tom Hanks".equals(StarName_MovieName[0])) System.out.println("Tom Hanks found");
+                if(!oldStars.containsKey(StarName_MovieName[0])){
+                    String tmpStarId = prefix + (++number);
+                    starInsertStatement.setString(1, tmpStarId);
+                    starInsertStatement.setString(2, StarName_MovieName[0]);
+                    starInsertStatement.setInt(3,0);
+                    starInsertStatement.addBatch();
+
+                    oldStars.put(StarName_MovieName[0], 0);
+                    starNameId.put(StarName_MovieName[0], tmpStarId);
+
+                    tmpQueryStatement.setString(1, tmpStarId);
+                    tmpQueryStatement.setString(2, movieTitleId.get(StarName_MovieName[1]));
+                    tmpQueryStatement.addBatch();
+                }else{
+                    statement.setString(1, starNameId.get(StarName_MovieName[0]));
+                    statement.setString(2, movieTitleId.get(StarName_MovieName[1]));
+                    statement.addBatch();
+                }
+            }
+            else{
+                // maybe a new movie, not sure add to db or not
+                count++;
+
+//                System.out.println("movie title not found: " + myStarsInMovies.get(starName));
+            }
+        }
+        System.out.println("sum of movie title not fount: " + count + "\nmaybe a new movie, not sure add to db or not");
+
+
+        starInsertStatement.executeBatch();
+        tmpQueryStatement.executeBatch();
+        statement.executeBatch();
+        conn.commit();
+
+        System.out.println("insert into stars_in_movies complete!!!");
+
+
 
         result.close();
         statement.close();
