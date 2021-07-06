922c6ed8240fd38462604050b01798aa7fcd85e7
ZONGCHANG LYU <zongchanglyu@zongchangdembp.attlocal.net>
2021-07-05 18:28:58 -0700

    test git again

--- FabFlixWeb/src/AddNewMovieServlet.java
+++ FabFlixWeb/src/AddNewMovieServlet.java
@@ -17,7 +17,7 @@ import java.sql.ResultSet;
 
 @WebServlet(name = "AddNewMovie", urlPatterns = "/api/add-new-movie")
 public class AddNewMovieServlet extends HttpServlet {
-    private static final long serialVersionUID = 2L;
+    private static final long serialVersionUID = 3L;
 
     // Create a dataSource which registered in web.xml
     @Resource(name = "jdbc/master")
